4532fccf6b219022ba1b741ffb7bc5bf4bd946e1
Justin Workman <xtagon@gmail.com>
2020-04-24 20:30:49 -0700

    Add Tensorboard logging to the multi-agent example.

--- examples/example_multiagent_save.py
+++ examples/example_multiagent_save.py
@@ -10,14 +10,16 @@ timesteps_per_generation=100000
 # placeholder_env necessary for model to recognize,
 # the observation and action space, and the vectorized environment
 placeholder_env = BattlesnakeEnv(n_threads=num_threads, n_envs=num_envs)
-models = [PPO2(CustomPolicy, placeholder_env, verbose=1, learning_rate=1e-3) for _ in range(num_agents)]
+models = [PPO2(CustomPolicy, placeholder_env, verbose=1, learning_rate=1e-3, tensorboard_log="./tensorboard") for _ in range(num_agents)]
 # Close environment to free allocated resources
 placeholder_env.close()
 
-for _ in range(10):
+for generation in range(10):
     for (agent, model) in enumerate(models, start=1):
+        log_name = "gen{}_agent{}".format(generation, agent)
+        model_name = 'ppo2_agent{}'.format(agent)
         env = BattlesnakeEnv(n_threads=num_threads, n_envs=num_envs, opponents=[ m for m in models if m is not model])
         model.set_env(env)
-        model.learn(total_timesteps=timesteps_per_generation)
-        model.save('ppo2_agent{}'.format(agent))
+        model.learn(total_timesteps=timesteps_per_generation, tb_log_name=log_name)
+        model.save(model_name)
         env.close()
